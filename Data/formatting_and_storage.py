833f887003407f79c1a72bac4e96c3665e148762
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2020-12-04 10:37:52 +0200

    Added functions for Writting to and reading from TFRecord files

--- Data/formatting_and_storage.py
+++ Data/formatting_and_storage.py
@@ -1,5 +1,6 @@
 """
-Converts data into byte strings that are stored in TFRecord files
+Methods for converting data into byte strings and for writting serialized
+data into TFRecord files.
 
 """
 
@@ -25,10 +26,72 @@ def _int64_feature(value):
 # be serialized to a binary-string using the .SerializaToString method
 
 
- def serialize_example(list_of_features):
+ def serialize_example(feature0, feature1, feature2, feature3):
  	"""
   	Creates a tf.train.Example message ready to be written to a file.
   	"""
   	# Create a dictionary mapping the feature name to the tf.train.Example-compatible
   	# data type.
+  	feature = {
+  		"feature0": _int64_feature(feature0),
+  		"feature1": _int64_feature(feature1),
+  		"feature2":, _bytes_feature(feature2)
+  		"feature3":, _float_feature(feature3)
+  	}
+
+  	proto_message = tf.train.Example(features=tf.train.Features(
+  										feature=feature))
+  	return proto_message.SerializaToString()
+
+
+
+def tf_serialize_example(f0, f1, f2, f3):
+	"""
+	Wrapper for the serialize_example function so it can be used as 
+	as argument to .map() Dataset function
+	"""
+	tf_string = tf.py_function(
+		serialize_example,
+		(f0, f1, f2, f3), # pass these args to the above function
+		tf.string # the return type
+		)
+	return tf.reshape(tf_string, ())
+
+
+
+# def generator():
+#   for features in features_dataset:
+#     yield serialize_example(*features)
+
+
+# serialized_features_dataset = tf.data.Dataset.from_generator(
+#     generator, output_types=tf.string, output_shapes=())
+
+
+# Using TFRecordDataset can be useful for standardizing input data
+# optimizing perfomance
+
+def write_to_tfrecord(file_name, serialized_features_dataset):
+	"""
+	Wrties data to a TFRecord file.
+
+	"""
+	filename = file_name
+	writer = tf.data.experimental.TFRecordWriter(filename)
+	writer.write(serialized_features_dataset)
+	return
+
+
+def read_from_tfrecord(filenames):
+	raw_datasets = []
+	for file in filenames:
+		raw_datasets.append(tf.data.TFRecordDataset)
+	return raw_datasets
+
+
+
+
+
+
+
 
