8e385a2c5290dba20dae7cf1e9522975e482ecc6
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2020-12-17 18:42:55 +0200

    base model class

--- lab1/language_model/models/base.py
+++ lab1/language_model/models/base.py
@@ -1,5 +1,4 @@
-""" Model class to be extended by specific types of models"""
-
+""" Model class, to be extended by specific types of models. """
 # pylint: disable=missing-function-docstring
 from pathlib import Path
 from typing import Callable, Dict, Optional
@@ -8,22 +7,18 @@ from tensorflow.keras.models import Model as KerasModel
 from tensorflow.keras.optimizers import RMSprop
 import numpy as np
 
-from text_recognizer.datasets.dataset_sequence import DatasetSequence
-
+from language_model.datasets.dataset_sequence import DatasetSequence
 
 DIRNAME = Path(__file__).parents[1].resolve() / "weights"
 
-
 class Model:
-
-    """Base class, to be subclassed by predictors for specific type of data."""
-
+    """ Base class, to be subclassed by predictors for specific types of data """
     def __init__(
         self,
         dataset_cls: type,
         network_fn: Callable[..., KerasModel],
         dataset_args: Dict = None,
-        network_args: Dict = None,
+        network_args: Dict = None,        
     ):
         self.name = f"{self.__class__.__name__}_{dataset_cls.__name__}_{network_fn.__name__}"
 
@@ -36,12 +31,10 @@ class Model:
         self.network = network_fn(self.data.input_shape, self.data.output_shape, **network_args)
         self.network.summary()
 
-        self.batch_augment_fn: Optional[Callable] = None
-        self.batch_format_fn: Optional[Callable] = None
-
     @property
     def image_shape(self):
         return self.data.input_shape
+
 
     @property
     def weights_filename(self) -> str:
@@ -49,12 +42,16 @@ class Model:
         return str(DIRNAME / f"{self.name}_weights.h5")
 
     def fit(
-        self, dataset, batch_size: int = 32, epochs: int = 10, augment_val: bool = True, callbacks: list = None,
+        self, dataset, batch_size: int = 32,
+        epochs: int = 10, augment_val: bool = True, 
+        callbacks: list = None
     ):
         if callbacks is None:
             callbacks = []
-
-        self.network.compile(loss=self.loss(), optimizer=self.optimizer(), metrics=self.metrics())
+        
+        self.network.compile(loss=self.loss(), optimizer=self.optimizer(),
+                            metrics=self.metrics
+        )
 
         train_sequence = DatasetSequence(
             dataset.x_train,
@@ -63,12 +60,13 @@ class Model:
             augment_fn=self.batch_augment_fn,
             format_fn=self.batch_format_fn,
         )
+
         test_sequence = DatasetSequence(
             dataset.x_test,
             dataset.y_test,
             batch_size,
             augment_fn=self.batch_augment_fn if augment_val else None,
-            format_fn=self.batch_format_fn,
+            format_fn=self.batch_format_fn
         )
 
         self.network.fit(
@@ -81,19 +79,19 @@ class Model:
             shuffle=True,
         )
 
-    def evaluate(self, x: np.ndarray, y: np.ndarray, batch_size: int = 16, _verbose: bool = False):
+    def evaluate(self, x: np.ndarray, y: np.ndarray, batch_size: int = 16, verbose: bool = True):
         # pylint: disable=unused-argument
-        sequence = DatasetSequence(x, y, batch_size=batch_size)  # Use a small batch size to use less memory
+        sequence = DatasetSequence(x, y, batch_size=batch_size)
         preds = self.network.predict(sequence)
         return np.mean(np.argmax(preds, -1) == np.argmax(y, -1))
 
-    def loss(self):  # pylint: disable=no-self-use
+    def loss(self):
         return "categorical_crossentropy"
 
-    def optimizer(self):  # pylint: disable=no-self-use
+    def optimizer(self):
         return RMSprop()
 
-    def metrics(self):  # pylint: disable=no-self-use
+    def metrics(self):
         return ["accuracy"]
 
     def load_weights(self):
