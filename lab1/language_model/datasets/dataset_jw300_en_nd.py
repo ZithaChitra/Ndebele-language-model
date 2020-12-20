bc7bfb59c41f38302861e400736cd802560dc6d2
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2020-12-20 14:17:45 +0200

    function for loading and preprocessing jw300 datasets

---
+++ lab1/language_model/datasets/dataset_jw300_en_nd.py
@@ -0,0 +1,55 @@
+# dataset-jw300-en-nd.py
+
+from
+
+
+from language_model.datasets.dataset import Dataset
+
+class DatasetJW300(Dataset):
+	""" 
+	Class for datasets downloaded from the JW300 projecct
+	"""
+	
+	def load_or_generate(self):
+		""" returns self.data """
+		pass
+
+
+	@classproperty
+	def input_shape(self):
+		""" returns the shape of self.data """
+		pass
+
+	@classproperty
+	def text_to_token_sequences(self, token="word"):
+		"""
+		Converts self.data into sequences of tokens.
+		returns self.data_sequence 
+		"""
+		pass
+
+	def remove_duplicates(self):
+		"""
+		removes duplicates from self.data
+		return self.data (almost) without duplicates 
+		"""
+		pass
+
+
+	def normalize(self):
+		""" Normalize self.data """
+		
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
