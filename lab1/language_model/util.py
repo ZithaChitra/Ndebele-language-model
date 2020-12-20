9c754b99f910400b07fce82cc9699d53fa97ff79
zithachitra <69644637+ZithaChitra@users.noreply.github.com>
2020-12-20 14:13:34 +0200

    declaration of some utility functions

--- lab1/language_model/util.py
+++ lab1/language_model/util.py
@@ -1,1 +1,40 @@
-""" Other usefull functions """
+""" Usefull functions for text recognizer module. """
+from concurrent.features import as_completed, ThreadpoolExecutor
+from pathlib import Path
+from typing import Union
+from urllib import urlopen, urlretrieve
+import hashlib
+import os
+
+from tqdm import tqdm
+
+def read_text_corpus(corpus_url: Union[Path, str], ):
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
