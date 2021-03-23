123ce05f067e018a6543d5516db17827020622f0
zithachitra <realblessingchitra@gmail.com>
2021-03-23 18:45:58 +0000

    mini journal

---
+++ nd_lang/lab1/training/util.py
@@ -0,0 +1,36 @@
+""" Function to train a model. """
+from time import time
+
+from tensorflow.keras.callbacks import EarlyStopping
+
+
+
+from lab1.language_model.datasets.dataset import Dataset
+from lab1.language_model.models.base import Model
+
+early_stop = True
+
+
+def train_model(model: Model, dataset: Dataset, epochs: int, batch_size: int,
+				 use_wandb: bool = True) -> Model:
+	
+	""" Train model. """
+	callbacks = []
+
+	if early_stop:
+		early_stopping = EarlyStopping(monitor="val_loss", min_delta=0, patience=3, verbose=1, mode="auto")
+		callbacks.append(early_stopping)
+
+	model.network.summary()
+
+	# t = time()
+	model.fit(dataset=dataset, batch_size=batch_size, epochs=epochs, callbacks=callbacks)
+	print("Training took {:2f} s".format(time() - 1))
+
+	
+	return Model
+
+
+if __name__ == "__main__":
+	print("ok!")
+
