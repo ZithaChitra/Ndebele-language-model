43d5ef58d52f2e603fe696d8651701341ccbc632
zithachitra <realblessingchitra@gmail.com>
2021-03-26 13:18:54 +0000

    mlp update

--- nd_lang/lab1/training/run_experiment2.py
+++ nd_lang/lab1/training/run_experiment2.py
@@ -3,6 +3,7 @@ import importlib
 # from typing import Dict
 # import os
 import click
+import mlflow
 
 from lab1.training.util import train_model
 
@@ -20,7 +21,6 @@ def run_experiment(dataset, network, model, epoch, train_args):
 	
 	
 	print(f"Running experiment with network '{network}' and dataset '{dataset}''")
-
 	datasets_module = importlib.import_module("lab1.language_model.datasets.house_pred")
 	dataset_class_ = getattr(datasets_module, dataset)
 	# dataset_args = experiment_config.get("dataset_args", {})
@@ -33,15 +33,23 @@ def run_experiment(dataset, network, model, epoch, train_args):
 	networks_module = importlib.import_module("lab1.language_model.networks.mlp")
 	network_fn = getattr(networks_module, network)
 	# network_args = experiment_config.get("network_args", {})
-	model = model_class_(dataset_cls=dataset_class_, network_fn=network_fn)
-	
 
-	train_model(
-		model,
-		dataset,
-		epochs=epoch,
-		# batch_size=experiment_config["train_args"]["batch_size"]
-		)
+	mlflow.set_tracking_uri("sqlite:///mlruns.db")
+
+	with mlflow.start_run:
+		mlflow.log_param("dataset", dataset)
+		mlflow.log_param("network", network)
+		mlflow.log_param("model", model)
+	
+		model = model_class_(dataset_cls=dataset_class_, network_fn=network_fn)
+		
+
+		train_model(
+			model,
+			dataset,
+			epochs=epoch,
+			# batch_size=experiment_config["train_args"]["batch_size"]
+			)
 
 
 
